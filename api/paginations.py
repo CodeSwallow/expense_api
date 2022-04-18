f51c4f13bf5e67da547c446820fcaf60076237e1
Isai Ramirez <isai.ramirez.stamaria15@gmail.com>
2022-04-18 08:26:15 -0500

    Increased SmallResultsSetPagination from 5 to 10 pages

--- api/paginations.py
+++ api/paginations.py
@@ -14,6 +14,6 @@ class StandardResultsSetPagination(PageNumberPagination):
 
 
 class SmallResultsSetPagination(PageNumberPagination):
-    page_size = 5
+    page_size = 10
     page_size_query_param = 'page_size'
-    max_page_size = 10
+    max_page_size = 100
